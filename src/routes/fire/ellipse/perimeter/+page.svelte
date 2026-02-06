dbb2b9e7b46264a5d9a05ef29654b97b7006e4d6
cbevins <cbevins@montana.com>
2026-02-05 20:12:01 -0800

    Improved fire/ellipse/perimeter/+page.svelte

--- src/routes/fire/ellipse/perimeter/+page.svelte
+++ src/routes/fire/ellipse/perimeter/+page.svelte
@@ -4,53 +4,54 @@
     import {DagNodeTable} from '$lib/dag/DagNodeTable.svelte'
     import {nodesTable} from '$lib/dag/DagTables.js'
     
-    let lwr = $state(2)
-    let headDeg = $state(360)
-    let headRos = $state(1)
-    let time = $state(100)
-    let deg = $state(15)
+    let lwrInput = 2
+    let headDegInput = 45
+    let headRosInput = 1
+    let timeInput = 100
+    let deg = 15
 
-    // Create FireEllipseMod and select nodes
+    // Create a FireEllipseMod with beta-psi-theta vector angle from 'head' and ready()
     let src='head'  // 'head' or 'north'
-    const e = new FireEllipseMod('e', src)
-    e.setConsumers()
-
-    const {beta, psi, theta, center, ignition} = e
+    const e = new FireEllipseMod('e', src).ready()
+    const {beta, center, head, ignition, lwr, psi, theta, time} = e
     for(let v of [beta, psi, theta]) {
-        for(let node of [v.vhr, v.perim.head.x, v.perim.head.y, v.beta, v.psi, v.theta]) {
+        for(let node of [v.perim.head.x, v.perim.head.y, v.beta, v.psi, v.theta, v.vhr])
             node.select()
-        }
     }
+    center.head.x.select()
+    center.head.y.select()
 
     // Get and set required inputs
     const inputNodes = e.sortNodes(e.activeInputNodes())
-    e.head.angle.north.set(headDeg)
-    e.head.ros.set(headRos)
-    e.lwr.set(lwr)
-    e.time.set(time)
+    head.angle.north.set(headDegInput)
+    head.ros.set(headRosInput)
+    lwr.set(lwrInput)
+    time.set(timeInput)
 
+    // Get lists of active inputs, selected nodes, and active nodes
     const activeInputNodes = e.sortNodes(e.activeInputNodes())
     const selectedNodes = e.sortNodes(e.selectedNodes())
     const activeNodes = e.sortNodes(e.activeNodes())
 
     // Determine perimeter points at 'deg' degree intervals of beta, theta, and psi
-    const betaPts = []
-    const psiPts = []
-    const thetaPts = []
-    function fmt(node) { return node.value.toFixed(2) }
-    // REMEMBER - THESE ARE DEGREES FROM ***NORTH***, NOT HEAD
-    for(let i=0; i<=360; i+=deg) {
-        beta.angle[src].set(i)
-        psi.angle[src].set(i)
-        theta.angle[src].set(i)
-        e.updateAll()
-        betaPts.push([i, fmt(beta.perim.head.x), fmt(beta.perim.head.y), fmt(beta.beta), fmt(beta.psi), fmt(beta.theta), fmt(beta.vhr)])
-        psiPts.push([i, fmt(psi.perim.head.x), fmt(psi.perim.head.y), fmt(psi.beta), fmt(psi.psi), fmt(psi.theta), fmt(psi.vhr)])
-        thetaPts.push([i, fmt(theta.perim.head.x), fmt(theta.perim.head.y), fmt(theta.beta), fmt(theta.psi), fmt(theta.theta), fmt(theta.vhr)])
+    const betaPts = perimeterPoints(e, beta, src, deg)
+    const psiPts = perimeterPoints(e, psi, src, deg)
+    const thetaPts = perimeterPoints(e, theta, src, deg)
+    
+    function  perimeterPoints(e, vector, src, deg) {
+        function fmt(node) { return node.value.toFixed(8) }
+        const pts = []
+        for(let i=0; i<=360; i+=deg) {
+            vector.angle[src].set(i)
+            e.updateAll()
+            pts.push([i, fmt(vector.perim.head.x), fmt(vector.perim.head.y),
+                fmt(vector.beta), fmt(vector.psi), fmt(vector.theta), fmt(vector.vhr)])
+        }
+        return pts
     }
+
     const beta0 = betaPts[0]
     const theta0 = thetaPts[0]
-    console.log('theta at 0 deg:', theta0)
     const headers = ['Deg', 'Head X', 'Head Y', 'Beta', 'Psi', 'Theta', 'Vhr']
 
     const scope = {
@@ -69,15 +70,16 @@
         x: function(east) { return (east - this.west()) * this.scale()},
         y: function(north) { return (this.north() - north) * this.scale()},
     }
-    // console.log('scale',scope.scale(), 'zoom=', scope.zoom)
-    // console.log('west=',scope.west(), 'east=',scope.east())
-    // console.log('north=',scope.north(), 'south=',scope.south())
-    // console.log('geo [0,0] is [x,y]', scope.x(0), scope.y(0))
-    // console.log('geo [50,50] is [x,y]', scope.x(50), scope.y(50))
-    // console.log('ellipse center is', center.head.x.value, center.head.y.value)
 </script>
 
 <div class='ml-4 mt-4 mb-4'>
+    <div class='text-xl'>Fire Ellipse Perimeter</div>
+    <div class='text-normal'>
+        Example using FireEllipseMod to determine the ellipse perimeter points
+        plotted at uniform intervals of {deg} degrees of beta (red), theta (yellow),
+        or psi (blue) from '{src}'.  SvgScope is used to display the ellipse
+        in either a Cartesian or geographic coordinate system.
+    </div>
     <svg width={scope.view.width} height={scope.view.height}>
         <rect width={scope.view.width} height={scope.view.height} fill='green'/>
         <!-- Grid axis -->
@@ -91,18 +93,21 @@
             <circle cx={scope.x(e)} cy={scope.y(n)} r=2 fill='yellow'/>
         {/each}
         {#each psiPts as [deg, e, n, b, p, t]}
-            <!-- {console.log('psi deg', deg, e, n)} -->
             <circle cx={scope.x(e)} cy={scope.y(n)} r=1 fill='blue'/>
         {/each}
-        <!-- Center pt and line to theta at 0 degrees north -->
-        <circle cx={scope.x(center.head.x.value)} cy={scope.y(center.head.y.value)} r=2 fill='yellow'/>
+        <!-- Ignition point is the Cartesian origin -->
+        <circle cx={scope.x(ignition.head.x.value)} cy={scope.y(ignition.head.y.value)} r=4 fill='red'/>
+        <!-- Ellipse center pt in Cartesian coords -->
+        <circle cx={scope.x(center.head.x.value)} cy={scope.y(center.head.y.value)} r=3 fill='yellow'/>
+        <!-- Line from ellipse center to theta at 0 degrees from head -->
         <line x1={scope.x(center.head.x.value)} y1={scope.y(center.head.y.value)}
-              x2={scope.x(theta0[1])} y2={scope.y(theta0[2])} stroke='yellow'/>
+            x2={scope.x(theta0[1])} y2={scope.y(theta0[2])} stroke='yellow' stroke-width=3/>
+        <!-- Perimeter point for theta=0 (should be ellipse head) -->
         <circle cx={scope.x(theta0[1])} cy={scope.y(theta0[2])} r=4 fill='yellow'/>
-
-        <circle cx={scope.x(ignition.head.x.value)} cy={scope.y(ignition.head.y.value)} r=2 fill='red'/>
+        <!-- Line from ignition point to beta at 0 degrees from head -->
         <line x1={scope.x(ignition.head.x.value)} y1={scope.y(ignition.head.y.value)}
-              x2={scope.x(beta0[1])} y2={scope.y(beta0[2])} stroke='red'/>
+            x2={scope.x(beta0[1])} y2={scope.y(beta0[2])} stroke='red'/>
+        <!-- Perimeter point for beta=0 (should be ellipse head) -->
         <circle cx={scope.x(beta0[1])} cy={scope.y(beta0[2])} r=3 fill='red'/>
     </svg>
 </div>

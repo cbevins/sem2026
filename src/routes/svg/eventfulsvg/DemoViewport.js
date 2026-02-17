1a74b773573a0f95f17380a158c04c1d5eb8fe33
cbevins <cbevins@montana.com>
2026-02-17 10:15:41 -0800

    EventfulSvg demo works great!

--- src/routes/svg/eventfulsvg/DemoViewport.js
+++ src/routes/svg/eventfulsvg/DemoViewport.js
@@ -5,20 +5,24 @@ export class DemoViewport extends EventfulViewport {
         super(svgWidth, svgHeight, 200, 200,
             [16, 8, 4, 2, 1, 0.5, 0.1, 0.05], 4, 'ft',
             0, 400, 0, 400)
+        // Additional props manipulated by parent classes or components
+        this.lwRatio = 1
+        this.bearing = 0
     }
 
     drawSvg() {
-        const rx = this.width / 4
-        const c1 = this.width / 3
-        const c2 = 2 * this.width / 3
-        const c3 = this.width /  2
-        const y1 = this.height / 3
-        const y2 = 2 * this.height / 3
-
+        const rx = this.pd(this.width / 4)
+        const c1 = this.px(this.width / 3)
+        const c2 = this.px(2 * this.width / 3)
+        const c3 = this.px(this.width /  2)
+        const y1 = this.py(this.height / 3)
+        const y2 = this.py(2 * this.height / 3)
+        const ry = this.pd(rx / this.lwRatio)
+        const tr = `transform="rotate(${this.bearing}, ${c1}, ${y1})"`
         let str = `<rect x=${this.px(this.wxMin)} y=${this.py(this.wyMax)} width=${this.pd(this.width)} height=${this.pd(this.height)} fill='gray'/>`
-        str += `<circle cx=${this.px(c1)} cy=${this.py(y1)} r=${this.pd(rx)} fill='red' opacity="0.6"/>`
-        str += `<circle cx=${this.px(c2)} cy=${this.py(y1)} r=${this.pd(rx)} fill='yellow' opacity="0.6"/>`
-        str += `<circle cx=${this.px(c3)} cy=${this.py(y2)} r=${this.pd(rx)} fill='blue' opacity="0.6"/>`
+        str += `<ellipse cx=${c1} cy=${y1} rx=${rx} ry=${ry} fill='red' opacity="0.6" transform="rotate(${this.bearing}, ${c1}, ${y1})"/>`
+        str += `<ellipse cx=${c2} cy=${y1} rx=${rx} ry=${ry} fill='yellow' opacity="0.6" transform="rotate(${this.bearing}, ${c2}, ${y1})"/>`
+        str += `<ellipse cx=${c3} cy=${y2} rx=${rx} ry=${ry} fill='blue' opacity="0.6" transform="rotate(${this.bearing}, ${c3}, ${y2})"/>`
 
         const textAttr = "stroke='black' font-size=8 'font-family'='sans-serif' font-weight='light'"
         str += this.drawZoomCenterLabels(textAttr)

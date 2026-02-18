63138a95eaa4512bf21df50641d7e1c3a03a5a57
cbevins <cbevins@montana.com>
2026-02-17 16:53:17 -0800

    Added ClassName, FileName, and P svelte markup

---
+++ src/lib/markup/FileName.svelte
@@ -0,0 +1,7 @@
+<script>
+    let {children} = $props()
+</script>
+
+<span class='font-mono px-1 border rounded bg-gray-500 text-white'>
+    {@render children?.()}
+</span>
---
+++ src/lib/markup/P.svelte
@@ -0,0 +1,7 @@
+<script>
+    let {children} = $props()
+</script>
+
+<div class='ml-4 mt-4 text-normal'>
+    {@render children?.()}
+</div>
